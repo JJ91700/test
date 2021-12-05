033e479961eb8d82dce52dd82d5dc7dcc4d4e0bc
miekyarthur <1319624130@qq.com>
2021-12-05 17:10:35 +0800

    <A>[Util模块]：1. SMSUtil 新增短信登录，目前只能做到打印方法；

--- src/main/java/com/kaikeba/util/SMSUtil.java
+++ src/main/java/com/kaikeba/util/SMSUtil.java
@@ -20,6 +20,12 @@ public class SMSUtil {
         return true;
     }
 
+    public static boolean login(String phoneNumber,String code) {
+        System.out.println("phoneNumber = " + phoneNumber);
+        System.out.println("code = " + code);
+        return true;
+    }
+
     public static boolean sendBySMS(String phoneNumber,String code) {
 //        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "秘钥id", "秘钥值");
         DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI4GJVun7NhPS8FMgSqqS4", "TgTLk8KVJjESFu6iC1jTHsVMK53z5Q");
@@ -57,4 +63,43 @@ public class SMSUtil {
         }
         return false;
     }
+
+    public static boolean loginBySMS(String phoneNumber,String code) {
+//        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "秘钥id", "秘钥值");
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI4GJVun7NhPS8FMgSqqS4", "TgTLk8KVJjESFu6iC1jTHsVMK53z5Q");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setSysMethod(MethodType.POST);
+        request.setSysDomain("dysmsapi.aliyuncs.com");
+        request.setSysVersion("2017-05-25");
+        request.setSysAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+//        request.putQueryParameter("PhoneNumbers", "手机号");
+        request.putQueryParameter("PhoneNumbers", phoneNumber);
+//        request.putQueryParameter("SignName", "签名名称");
+        request.putQueryParameter("SignName", "快递e栈");
+//        request.putQueryParameter("TemplateCode", "短信模板code");
+        request.putQueryParameter("TemplateCode", "SMS_197625930");         // 只需要更新这个TemplateCode的值
+//        request.putQueryParameter("TemplateParam", "填充的参数（JSON对象格式）");
+        request.putQueryParameter("TemplateParam", "{\"code\":\""+code+"\"}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+            String json = response.getData();
+            Gson g = new Gson();
+            HashMap result = g.fromJson(json, HashMap.class);
+            if("OK".equals(result.get("Message"))) {
+                return true;
+            }else{
+                System.out.println("短信发送失败，原因："+result.get("Message"));
+            }
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
