1ff851f52638d8b96d4dcc7a761a68a0c97ad043
miekyarthur <1319624130@qq.com>
2021-12-11 19:08:47 +0800

    筛选器限制未登录用户登录网址index.html； <A>[wx登录模块]：1. 新增筛选器，当前用户/快递员没有登录成功，则session中没有保存对应的user或者courier，是不允许进入index.html，并且跳转到login.html；

---
+++ src/main/java/com/kaikeba/filter/WxUserAccessControlFilter.java
@@ -0,0 +1,35 @@
+package com.kaikeba.filter;
+
+import com.kaikeba.bean.Courier;
+import com.kaikeba.bean.User;
+import com.kaikeba.util.UserUtil;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@WebFilter(filterName = "WxUserAccessControlFilter")
+@WebFilter("/index.html")
+public class WxUserAccessControlFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        User user = UserUtil.getWxUser(req.getSession());
+        Courier courier = UserUtil.getWxCourier(req.getSession());
+        if (user != null || courier != null) {
+            chain.doFilter(request, response);
+        } else {
+            req.getSession().invalidate();
+            resp.sendRedirect("login.html");
+        }
+    }
+}
